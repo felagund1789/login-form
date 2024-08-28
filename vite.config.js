a0a013ceac59eb57100e1978ca6a743397ba68a7
Konstantinos Kokordelis <kokordelis.konstantinos@gmail.com>
2024-08-28 07:54:12 +0300

    First commit. Signup form.

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        app: "./signup.html"
+      }
+    }
+  },
+  server: {
+    open: "./signup.html"
+  }
+})
