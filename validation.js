0a58ebc93a6c461c5d763d889fef38b70f2193f0
Konstantinos K <raistlindragonl@gmail.com>
2024-08-28 13:06:09 +0300

    Implement form validation

---
+++ validation.js
@@ -0,0 +1,44 @@
+const form = document.getElementById("form");
+const firstname_input = document.getElementById("firstname-input");
+const email_input = document.getElementById("email-input");
+const password_input = document.getElementById("password-input");
+const repeat_password_input = document.getElementById("repeat-password-input");
+const error_message = document.getElementById("error-message");
+
+form.addEventListener("submit", (e) => {
+  let errors = getFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value);
+
+  if (errors.length > 0) {
+    // If there are any errors
+    e.preventDefault();
+    error_message.innerText = errors.join(". ");
+  }
+});
+
+function getFormErrors(firstname, email, password, repeatPassword) {
+  let errors = [];
+
+  if (firstname === "" || firstname == null) {
+    errors.push("Firstname is required");
+    firstname_input.parentElement.classList.add("incorrect");
+  }
+  if (email === "" || email == null) {
+    errors.push("Email is required");
+    email_input.parentElement.classList.add("incorrect");
+  }
+  if (password === "" || password == null) {
+    errors.push("Password is required");
+    password_input.parentElement.classList.add("incorrect");
+  }
+  if (password.length < 8) {
+    errors.push("Password must have at least 8 characters");
+    password_input.parentElement.classList.add("incorrect");
+  }
+  if (password !== repeatPassword) {
+    errors.push("Password does not match repeated password");
+    password_input.parentElement.classList.add("incorrect");
+    repeat_password_input.parentElement.classList.add("incorrect");
+  }
+
+  return errors;
+}
